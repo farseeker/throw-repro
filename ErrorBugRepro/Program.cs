bcbf19155b5f02843c54e980325b3de0b8852fa3
Mark Henderson <mark.henderson@take2games.com>
2020-06-12 09:10:19 +0930

    Throw this

---
+++ ErrorBugRepro/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ErrorBugRepro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Run();
+        }
+        async void Run()
+        {
+            int runs = 0;
+            do
+            {
+                runs++;
+                Console.WriteLine(runs);
+                try
+                {
+                    Task myTask = new Task(() => throwException());
+                    myTask.Start();
+                    myTask.Wait();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Thread.Sleep(1000);
+            } while (true);
+        }
+
+        async Task throwException()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
